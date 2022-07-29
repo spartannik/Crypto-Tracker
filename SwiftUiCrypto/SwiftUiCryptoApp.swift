ae6fe011ac05d595844f8a742c5a93f198d6dfe5
spartannik <dead5042@gmail.com>
2022-07-29 19:35:19 +0300

    Initial Commit

---
+++ SwiftUiCrypto/SwiftUiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCryptoApp.swift
+//  SwiftUiCrypto
+//
+//  Created by Nikita Yashchenko on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
