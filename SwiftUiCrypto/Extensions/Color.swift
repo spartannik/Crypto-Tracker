3d859f0b92f64a4fd2da7484ae3f39cc2f43581b
spartannik <dead5042@gmail.com>
2022-07-29 20:07:18 +0300

    1

---
+++ SwiftUiCrypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  SwiftUiCrypto
+//
+//  Created by Nikita Yashchenko on 29.07.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
+
