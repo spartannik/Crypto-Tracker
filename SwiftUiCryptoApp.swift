65ae70319a3367c5f4ef0fc7005457617144d69d
spartannik <dead5042@gmail.com>
2022-07-30 17:58:51 +0300

    stage 2

--- SwiftUiCryptoApp.swift
+++ SwiftUiCryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftUiCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
